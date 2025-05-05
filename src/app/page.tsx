8998e0f49525c58896e8c80be9f5a1319461392d
deepak29-git <dg887180@gmail.com>
2025-05-05 16:21:53 +0530

    change in pre-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div>
-      <h1>Home hello world 1</h1>
+      <h1>Home hello world 2</h1>
       <Link href="/about">About deepak</Link>
     </div>
   );
